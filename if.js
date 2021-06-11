02c310a3e5d38fde7a2cb4dd847e9a11e66faeb4
May <edmay3111@gmail.com>
2021-06-11 19:49:11 +0630

    first commit

---
+++ if.js
@@ -0,0 +1,9 @@
+const age = 22;
+if ( age < 18)
+{
+	console.log("underage");
+}
+else  
+{
+	console.log ("adult")
+}
---
+++ switch.js
@@ -0,0 +1,16 @@
+
+const fruit = "Strawberry";
+
+switch(fruit){
+    case 'Strawberry ':
+        console.console.log('sweet'); 
+        break;
+     case 'Orange':
+            console.console.log('Sour'); 
+            break;
+        default:
+        console.console.log('I like fruit');
+        break;
+
+}
+
